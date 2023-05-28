83caca1043ce92f785292e9c376551488b818489
junkingboy <junkingboy@163.com>
2023-05-29 02:03:40 +0800

    Add download file

--- HttpConsole/HttpConsole.cpp
+++ HttpConsole/HttpConsole.cpp
@@ -1,20 +1,70 @@
-﻿// HttpConsole.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<iostream>
+#include<Windows.h>
+#include<fstream>
+#include<string>
 
-#include <iostream>
+using namespace std;
 
-int main()
+/*
+	读取windows注册表中给定键的值
+	参数1:	要打开的基键
+	参数2: 要读取值的子键路径
+	参数3: 要读取的值的名称
+	使用 Windows API 函数 RegOpenKeyEx 和 RegQueryValueEx 来打开和读取子键。如果读取值成功，则返回该值的字符串形式；否则返回一个空字符串
+*/
+string regKeyValue(HKEY hKey, const string& subKey, const string& valueName)
 {
-    std::cout << "Hello World!\n";
+	HKEY hSubKey;
+	if (RegOpenKeyEx(hKey, subKey.c_str(), 0, KEY_READ, &hSubKey) != ERROR_SUCCESS)
+	{
+		return "";
+	}
+	
+	DWORD type;
+	DWORD dataSize;
+
+	if (RegQueryValueEx(hSubKey, valueName.c_str(), NULL, &type, NULL, &dataSize) != ERROR_SUCCESS || type != REG_SZ)
+	{
+		RegCloseKey(hSubKey);
+		return "";
+	}
+
+	char* data = new char[dataSize];
+	if (RegQueryValueEx(hSubKey, valueName.c_str(), NULL, NULL, (LPBYTE)data, &dataSize) != ERROR_SUCCESS)
+	{
+		RegCloseKey(hSubKey);
+		delete[] data;
+		return "";
+	}
+
+	string result(data);
+	RegCloseKey(hSubKey);
+	delete[] data;
+	return result;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+bool isGitForWindowsInstalled()
+{
+	string path = regKeyValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\GitForWindows", "InstallPath");
+	return !path.empty();
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+/* 下载文件回调函数 */
+static int downLoadFileCallBack(void* ptr, size_t sixe, size_t nmemb, void* stearm)
+{
+	size_t written = fwrite(ptr, sixe, nmemb, (FILE*)stearm);
+	return static_cast<int>(written);
+}
+
+/* 下载文件函数,libcurl库实现,由于url和fileName都是以字符串形式传入,所以处理的是字符串 */
+void downLoadFile(const char* url, const char* fileName)
+{
+
+}
+
+void downLoadGitForWindows()
+{
+	const char* url = "https://github.com/git-for-windows/git/releases/download/v2.40.1.windows.1/Git-2.40.1-64-bit.exe";
+	const char* fileName = "Git";
+	downLoadFile(url, fileName);
+}
